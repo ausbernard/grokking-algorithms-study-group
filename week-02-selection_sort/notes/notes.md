5789f5b3072a22d31e87df880f469c89f5aad765
ausbernard <109692469+ausbernard@users.noreply.github.com>
2025-12-02 15:33:29 -0500

    week 2 readme, notes, and problem sets

---
+++ week-02-selection_sort/notes/notes.md
@@ -0,0 +1,19 @@
+# Reading Questions
+
+1. 
+
+# Extra Questions
+1. a
+
+# Exercises
+Give the run time for each of these scenarios in terms of Big O.
+**1.3**: You have a name, and you want to find the person's phone number in the phone book (names are ordered): 
+**1.4**: You have a phone number, and you want to find the person's name in the phone book (phone numbers are un-ordered): 
+**1.5**: You want to read the numbers of every person in the phone: 
+**1.6**: You want to read the numbers of just A's: 
+
+# Extra Readings
+a
+
+# Meeting 
+**Big O Notation Cheat Sheet**
---
+++ week-02-selection_sort/problems/required/basic_selection_sort.py
@@ -0,0 +1,30 @@
+"""
+Sort an array from smallest to largest in O(n^2) using selection sort method. If there is no array, return -1
+"""
+
+def selection_sort(arr):
+    # Insert your code here
+    return -1
+
+def test(description, arr, expected):
+    result = selection_sort(arr)
+    try:
+        assert result == expected, f"Expected {expected}, got {result}"
+        print(f"PASS✅: {description}")
+        return True
+    except AssertionError as e:
+        print(f"FAIL❌: {description}")
+        print(f"    Input:  arr={arr}")
+        print(f"    {e}")
+    
+    return False
+            
+# Run tests
+print("Running first_last_position tests:\n")
+
+test("Base Case", [2,1,5,4,3], [1,2,3,4,5])
+test("Duplicates", [7,5,7,8,2,9], [2,5,7,7,8,9])
+test("Multiple Duplicates", [7,5,7,8,2,9], [2,5,7,7,8,9])
+test("Already Sorted", [1,2,3,4,5], [1,2,3,4,5])
+test("Empty array", [], -1)
+test("All same elements", [1,1,1,1,1], [1,1,1,1,1])
